0ede7332d7dbb5dce7fab5b9055d1b37c127509c
Justin Tompkins <jtompkins9045@gmail.com>
2021-11-09 20:36:47 -0500

    updated API

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const baseURL ='https://api.nasa.gov/';
+const baseURL ='https://api.nasa.gov/planetary/apod?api_key=bZFuTKNjpOZhRDEObqigsWPeYFw3KFWyFUTWhygh';
 
 const searchForm = document.querySelector('form');
 const nasa = document.querySelector('ul');
@@ -12,16 +12,24 @@ function fetchnasa(e){
     .then(result => result.json())
     .then(json => {
       console.log(json);
-        display(json);
+        displayPlanetary(json);
     })
     .catch(err => console.log(err))
 }
 
-function display(){
-        .forEach(d => {
-            let  = document.createElement('li');
-            .innerText = d.[1];
-            nasa.appendChild();
-        
-        })
-    }
+function displayPlanetary(planetary){
+    planetary.forEach(p =>{
+        let planetary = document.createElement('li');
+        nasa.innerText = p.title[1];
+        nasa.appendChild(planetary);
+    })
+}
+
+function displayPlanetary(planetary){
+    planetary.forEach(p => {
+        let planetary = document.createElement('li');
+        planetary.innerText = p.date[1]
+        nasa.appendChild(planetary);
+    })
+}
+
